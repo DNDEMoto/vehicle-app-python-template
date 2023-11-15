f49ebb95d5c0050c829ae4732d5bb7bec46ce115
moto <139359566+DNDEMoto@users.noreply.github.com>
2023-11-15 15:47:40 +0000

    adapt latest velocitas

--- .github/scripts/deploy_image_from_artifact.sh
+++ .github/scripts/deploy_image_from_artifact.sh
@@ -17,17 +17,17 @@
 set -e
 
 ROOT_DIRECTORY=$( realpath "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../.." )
-APP_ARTIFACT_NAME=$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq .[].name | tr -d '"')
+APP_ARTIFACT_NAME=$(cat $ROOT_DIRECTORY/app/AppManifest.json | jq -r .name | tr -d '"')
 APP_NAME_LOWERCASE=$(echo $APP_ARTIFACT_NAME | tr '[:upper:]' '[:lower:]')
 APP_PORT=50008
 APP_REGISTRY="k3d-registry.localhost:12345"
-RUNTIME_VERSION=$(cat $ROOT_DIRECTORY/.velocitas.json | jq -r '.packages[]| select(.name=="devenv-runtimes")'.version)
-HELM_CONFIG_DIR="$HOME/.velocitas/packages/devenv-runtimes/$RUNTIME_VERSION/runtime-k3d/src/app_deployment/config/helm"
+RUNTIME_VERSION=$(cat $ROOT_DIRECTORY/.velocitas.json | jq -r '.packages[]| select(.name | contains("devenv-runtimes"))'.version)
+HELM_CONFIG_DIR="$HOME/.velocitas/packages/devenv-runtimes/$RUNTIME_VERSION/runtime_k3d/src/app_deployment/config/helm"
 
 local_tag="$APP_REGISTRY/$APP_NAME_LOWERCASE:local"
 echo "Local URL: $local_tag"
 
-docker load -i "$APP_ARTIFACT_NAME.tar" | sed -n 's/^Loaded image ID: sha256:\([0-9a-f]*\).*/\1/p' | xargs -i docker tag {} $local_tag
+docker load -i "$APP_ARTIFACT_NAME.tar" | sed -n 's/^Loaded image: \([0-9a-f]*\).*/\1/p' | xargs -i docker tag {} $local_tag
 docker push $local_tag
 
 helm install vapp-chart $HELM_CONFIG_DIR \
