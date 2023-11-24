f732e16d9dfb3ff876b4e91a61981498737957ad
moto <139359566+DNDEMoto@users.noreply.github.com>
2023-11-24 16:03:43 +0000

    add simple speed listener

---
+++ app/src/speed_app.py
@@ -0,0 +1,40 @@
+
+from sdv.vehicle_app import VehicleApp
+from vehicle import Vehicle, vehicle
+from sdv.vdb.reply import DataPointReply
+import logging
+import asyncio
+import signal
+
+class SpeedApp(VehicleApp):
+    """Array Datatype example Vehicle App"""
+    speed = 0
+    def __init__(self, vehicle: Vehicle):
+        super().__init__()
+        self.Vehicle = vehicle
+
+    def get_speed(self) -> float:
+        return self.speed
+    
+    def print_values(self, data: DataPointReply):
+        speed = data.get(self.Vehicle.Speed).value
+        print("pf: velocitas: Vehicle Speed: {}".format(speed))
+        
+    async def on_start(self):
+        """Run when the vehicle app starts"""
+
+        await self.Vehicle.Speed.subscribe(
+            self.print_values
+        )
+
+
+async def main():
+    print("velocitas: speed app main")
+    speed_app = SpeedApp(vehicle)
+    await speed_app.run()
+
+
+LOOP = asyncio.get_event_loop()
+LOOP.add_signal_handler(signal.SIGTERM, LOOP.stop)
+LOOP.run_until_complete(main())
+LOOP.close()
