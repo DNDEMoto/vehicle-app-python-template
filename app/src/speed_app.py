cd25458ce9c51633363aa551921f19ff56ae4551
moto <139359566+DNDEMoto@users.noreply.github.com>
2023-11-30 16:23:53 +0000

    add sample code

--- app/src/speed_app.py
+++ app/src/speed_app.py
@@ -18,23 +18,19 @@ class SpeedApp(VehicleApp):
     
     def print_values(self, data: DataPointReply):
         speed = data.get(self.Vehicle.Speed).value
-        print("pf: velocitas: Vehicle Speed: {}".format(speed))
+        print("velocitas: Vehicle Speed: {}".format(speed))
         
     async def on_start(self):
         """Run when the vehicle app starts"""
-
-        await self.Vehicle.Speed.subscribe(
-            self.print_values
-        )
-
+        await self.Vehicle.Speed.subscribe(self.print_values)
 
 async def main():
     print("velocitas: speed app main")
     speed_app = SpeedApp(vehicle)
     await speed_app.run()
 
-
-LOOP = asyncio.get_event_loop()
-LOOP.add_signal_handler(signal.SIGTERM, LOOP.stop)
-LOOP.run_until_complete(main())
-LOOP.close()
+#
+# LOOP = asyncio.get_event_loop()
+# LOOP.add_signal_handler(signal.SIGTERM, LOOP.stop)
+# LOOP.run_until_complete(main())
+# LOOP.close()
---
+++ temp/hello2.py
@@ -0,0 +1,5 @@
+
+import asyncio
+
+def test():
+    print("test")
